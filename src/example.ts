0fd1384573f7118a7b01bc4272585d5d35f0dd23
Lam Nhan <lamnhan92@gmail.com>
2018-11-04 17:21:45 +0700

    add testes and ci

--- src/example.ts
+++ src/example.ts
@@ -8,8 +8,8 @@ export function example1(): void {
         b: 2,
         c: {
             c1: 1,
-            c2: 2
-        }
+            c2: 2,
+        },
     };
     const a = Utils.o2a(o);
     Logger.log(a);
@@ -33,9 +33,9 @@ export function example4(): void {
         a1: '1',
         b: true,
         b1: 'TRUE',
-        c: { c1: 1, c2: 2 }, 
+        c: { c1: 1, c2: 2 },
         c1: '{ "c1": 1, "c2": 2 }',
-        d: null
+        d: null,
     };
     const output = Utils.honorData(o);
     Logger.log(output);
